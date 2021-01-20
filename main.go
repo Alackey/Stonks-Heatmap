6e6cd2bb4e3075e7164a0ec9514f665e5043bcc5
Anthony Lackey <alackey96@gmail.com>
2021-01-19 21:24:33 -0800

    Grabs the image url instead of taking a screenshot

--- main.go
+++ main.go
@@ -1,68 +1,77 @@
 package main
 
 import (
-	"bytes"
 	"context"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	// Create options
+	// Setup browser
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.WindowSize(1000, 1000),
+		chromedp.WindowSize(1920, 1080),
 	)
 
 	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
 	defer cancel()
 
-	// create context
 	ctx, cancel := chromedp.NewContext(allocCtx)
 	defer cancel()
 
-	// Take screenshot
-	var buf []byte
-	if err := chromedp.Run(ctx, screenshot(`https://finviz.com/map.ashx?t=sec`, `.chart`, &buf)); err != nil {
-		log.Fatal(err)
+	// Get heatmap images
+	var stockURL string
+	if err := chromedp.Run(ctx, getStockMarketMap(&stockURL)); err != nil {
+		log.Fatalln("Error getting stock market heatmap:", err)
+		os.Exit(1)
 	}
 
-	// Upload image
-	if err := uploadImage(buf); err != nil {
-		log.Fatalf("Failed to upload heatmap image: %v", err)
+	// Update the database
+	err := updateURL("Stock Market", stockURL)
+	if err != nil {
+		log.Fatalln("Error updating the URL in the database:", err)
+		os.Exit(1)
 	}
 }
 
-// screenshot takes a screenshot of a specific element.
-func screenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
+// getStockMarketMap gets the stock market heatmap image url
+func getStockMarketMap(stockURL *string) chromedp.Tasks {
 	return chromedp.Tasks{
-		chromedp.Navigate(urlstr),
-		chromedp.WaitVisible(sel, chromedp.ByQuery),
-		chromedp.Screenshot(sel, res, chromedp.NodeVisible, chromedp.ByQuery),
+		chromedp.Navigate("https://finviz.com/map.ashx?t=sec"),
+		chromedp.WaitVisible(".chart", chromedp.ByQuery),
+		chromedp.Click("//*[@id=\"share-map\"]", chromedp.NodeVisible),
+		chromedp.Value("//*[@id=\"static\"]", stockURL, chromedp.NodeVisible),
 	}
 }
 
-// uploadImage uploads image to the heatmap S3 bucket
-func uploadImage(buf []byte) error {
+// updateURL calls updates the image url in DynamoDB
+func updateURL(key string, url string) error {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
 
-	uploader := s3manager.NewUploader(sess)
-
-	bucket := "stockbot-heatmap"
-	keyName := "marketHeatmap.png"
-	body := bytes.NewReader(buf)
-
-	upParams := &s3manager.UploadInput{
-		Bucket: &bucket,
-		Key:    &keyName,
-		Body:   body,
+	svc := dynamodb.New(sess)
+	input := &dynamodb.UpdateItemInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":u": {
+				S: aws.String(url),
+			},
+		},
+		Key: map[string]*dynamodb.AttributeValue{
+			"Name": {
+				S: aws.String(key),
+			},
+		},
+		TableName:        aws.String("Stonks_Heatmaps"),
+		UpdateExpression: aws.String("SET Image = :u"),
 	}
 
-	_, err := uploader.Upload(upParams)
+	_, err := svc.UpdateItem(input)
 	if err != nil {
 		return err
 	}
